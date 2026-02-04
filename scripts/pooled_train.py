5a3bd3f3869e35db5cd48651864aa3b5ffb94f89
ghezlene <154736153+ghezlen18@users.noreply.github.com>
2026-02-04 12:56:30 +0100

    final regression commit

--- scripts/pooled_train.py
+++
@@ -1,3 +0,0 @@
-def prepare_Xy_from_df(df):
-def eval_model(name, model, Xs, ys):
-print('This file has been moved to backups/regression_backup_2026-02-02/pooled_train.py')
--- scripts/split_datasets.py
+++
@@ -1,1 +0,0 @@
-print('This file has been moved to backups/regression_backup_2026-02-02/split_datasets.py')
